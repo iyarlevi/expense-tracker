90f7f99235416406741b81e4d0ce63d6a34896c0
iyar levi <iyarlevi5@gmail.com>
2025-04-03 15:38:08 +0300

    Add filter to the list

--- expense-tracker/src/pages/Home.jsx
+++ expense-tracker/src/pages/Home.jsx
@@ -3,13 +3,14 @@ import ExpenseForm from "../components/ExpenseForm";
 import ExpenseList from "../components/ExpenseList";
 
 const Home = () => {
-  const [expenses, setExpenses] = useState(() => {
-    // Load stored expenses when the app starts
-    return JSON.parse(localStorage.getItem("expenses")) || [];
-  });
+  const [filterCategory, setFilterCategory] = useState("");
+  const [expenses, setExpenses] = useState(
+    // Load the list from local storage
+    () => JSON.parse(localStorage.getItem("expenses")) || []
+  );
 
+  // Save the list from local storage
   useEffect(() => {
-    // Save expenses whenever they change
     localStorage.setItem("expenses", JSON.stringify(expenses));
   }, [expenses]);
 
@@ -17,11 +18,29 @@ const Home = () => {
   const deleteExpense = (id) =>
     setExpenses(expenses.filter((exp) => exp.id !== id));
 
+  // Filter the list base on the user filter input
+  const filteredExpenses = expenses.filter((exp) => {
+    return !filterCategory || exp.category === filterCategory;
+  });
+
   return (
     <div>
       <h1>Expense Tracker</h1>
+
+      <div className="filter-section">
+        <select
+          value={filterCategory}
+          onChange={(e) => setFilterCategory(e.target.value)}
+        >
+          <option value="">All Categories</option>
+          <option value="Food">Food</option>
+          <option value="Transport">Transport</option>
+          <option value="Entertainment">Entertainment</option>
+        </select>
+      </div>
+
       <ExpenseForm addExpense={addExpense} />
-      <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
+      <ExpenseList expenses={filteredExpenses} deleteExpense={deleteExpense} />
     </div>
   );
 };
